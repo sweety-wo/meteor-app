8813e1651de6f1ca1a9d9646dc636e903f9c2b66
Vipul Jain <vipul.jain0@gmail.com>
2016-09-09 12:19:22 +0530

    Sidebar message changes

--- client/views/navbar/navbar.js
+++ client/views/navbar/navbar.js
@@ -1,3 +1,14 @@
+var isUserExist = function (array, user) {
+    var isExist = false
+    for(var i = 0; i < array.length; i++){
+        if(user === array[i].username){
+            isExist = true;
+            return isExist;
+        }
+    }
+    return isExist;
+};
+
 Template.navbar.helpers({
 
     searchSettings: function() {
@@ -10,6 +21,49 @@ Template.navbar.helpers({
                     template: Template.usernameAutoComplete
                 }
             ]
+        }
+    }
+});
+
+Template.navbar.events({
+    'click .userLogout': function (event) {
+        event.preventDefault();
+        Meteor.logout();
+        Router.go('/');
+    },
+    'keyup #searchUser1': function (event, template) {
+        event.preventDefault();
+        var friend = template.existingFriend.get();
+        var array = template.friendList.get();
+        if(event.keyCode === 13) {
+            var user = $('#searchUser').val();
+            var userExist = Meteor.users.find({username: user}).fetch();
+            if(userExist && userExist.length > 0) {
+                console.log('user', user);
+                if (!friend) {
+                    friend = {};
+                    friend.createdBy = Meteor.user().username;
+                    if (!isUserExist(array, user)) {
+                        array.push({'username': user});
+                        friend.friends = array;
+                        Meteor.call('friend.insert', friend, function(err, result){
+                            if (result){
+                                friend._id = result;
+                            }
+                        });
+                        template.existingContact.set(contact);
+                        template.friendList.set(array);
+                    }
+                }else{
+                    if (!isUserExist(array, user)) {
+                        array.push({'username': user});
+                        friend.friends = array;
+                        template.existingFriend.set(friend);
+                        template.friendList.set(array);
+                        Meteor.call('friend.update', friend);
+                    }
+                }
+            }
         }
     }
 });
