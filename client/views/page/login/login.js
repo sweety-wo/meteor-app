9a9dfc0afa1fe3c8174c70d891a875b488bb1811
Vipul-jain <vipul.jain0@gmail.com>
2016-09-09 19:10:53 +0530

    Added validations

--- client/views/page/login/login.js
+++ client/views/page/login/login.js
@@ -7,21 +7,25 @@ Template.login.events({
     event.preventDefault();
     var emailVar = event.target.loginEmail.value;
     var passwordVar = event.target.loginPassword.value;
-    Meteor.loginWithPassword(emailVar, passwordVar, function (err) {
-      if(err){
-        if(err.reason === 'User not found'){
-          toastr.error('Login failed: ' + err.reason);
-          Router.go('signUp');
-        }else if(err.reason.indexOf('Please enter username') > -1){
-          console.log("hello")
-          var id = err.reason.replace('Please enter username ', '');
-          var user = Meteor.users.findOne({_id: id});
-        }else{
-          toastr.error('Login failed: ' + err.reason);
+    if(emailVar && passwordVar) {
+      Meteor.loginWithPassword(emailVar, passwordVar, function (err) {
+        if (err) {
+          if (err.reason === 'User not found') {
+            toastr.error('Login failed: ' + err.reason);
+            Router.go('signUp');
+          } else if (err.reason.indexOf('Please enter username') > -1) {
+            var id = err.reason.replace('Please enter username ', '');
+            var user = Meteor.users.findOne({_id: id});
+          } else {
+            toastr.error('Login failed: ' + err.reason);
+          }
+        } else {
+          Router.go('inbox');
         }
-      }else {
-        Router.go('inbox');
-      }
-    });
+
+      });
+    }else{
+      toastr.error("Please enter all information")
+    }
   },
 });
